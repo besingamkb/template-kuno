d6062e1d0d597ddd7178f26509f6742df3a7087b
Mark Kevin Besinga <markkevin.besinga@hcl.com>
2022-09-28 09:31:10 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    'static': {
+      directory: './dist'
+    }
+  }
+};
+
+module.exports = config;
